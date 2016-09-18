fcaa63d92bd1ed55b964e089041c6b33250c76c4
Shlok Kapoor <shlok_97@rocketmail.com>
2016-09-18 19:52:03 +0530

    Initial Commit

---
+++ NextTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextTime
+//
+//  Created by Shlok Kapoor on 18/09/16.
+//  Copyright © 2016 Shlok Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
